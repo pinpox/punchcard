2f88bafcaca5ec0f3f854b676a943ea9128e1005
pinpox <git@pablo.tools>
2026-03-10 13:42:48 +0100

    Add AGPL-3.0 license

--- flake.nix
+++ flake.nix
@@ -3,20 +3,27 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
       version = builtins.substring 0 8 lastModifiedDate;
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
-        {
+        rec {
           punchcard = pkgs.buildGoModule {
             pname = "punchcard";
             inherit version;
@@ -27,22 +34,26 @@
             meta = with pkgs.lib; {
               description = "A simple time tracking application";
               homepage = "https://github.com/pinpox/punchcard";
-              license = licenses.mit;
+              license = licenses.agpl3Only;
+              maintainers = [ maintainers.pinpox ];
               mainProgram = "punchcard";
             };
           };
-        });
+          default = punchcard;
+        }
+      );
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools sqlite ];
+      devShells = forAllSystems (
+        system: with nixpkgsFor.${system}; {
+          default = mkShell {
+            buildInputs = [
+              go
+              gopls
+              gotools
+              sqlite
+            ];
           };
-        });
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.punchcard);
+        }
+      );
     };
 }
